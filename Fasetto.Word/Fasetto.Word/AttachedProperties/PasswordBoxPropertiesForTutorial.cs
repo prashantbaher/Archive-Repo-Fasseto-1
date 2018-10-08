6942c2613ef38b35eb0c726f97d2303d7224a172
Prashant Baher <prashantbaher@gmail.com>
2018-10-08 14:29:31 +0530

    Complete the Attached Properties for password box
    Complete the Attached Properties for password box.
    1st we learn how attached property is done in Standard way.
    Then we learn how to use the generics to first create a BaseAttachedProperty class. Which is our base class for attached properties.
    Then we use our base class to create HasText property and MonitorPassword property.
    After doing these things, we hook up the visibility password box with these 2 propertes.
    But we run into an issue.
    Which is solved by using converter method.
    In this converter class we just use a boolean.

---
+++ Fasetto.Word/Fasetto.Word/AttachedProperties/PasswordBoxPropertiesForTutorial.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Fasetto.Word
+{
+    public class PasswordBoxPropertiesForTutorial
+    {
+        #region Standard Way of doint Attached Properties
+
+        /* This region is not going to stay in Luke's code. He used these codes
+         * just to show how attached properties are created in standard way.
+         * He has other ways to do the same thing so he is doint that way.
+         * Because of this, we did not have comment in this region.
+         * This is just for our information.
+         */
+
+        public static readonly DependencyProperty MonitorPasswordProperty =
+            DependencyProperty.RegisterAttached("MonitorPassword", typeof(bool), typeof(PasswordBoxPropertiesForTutorial), new PropertyMetadata(false, OnMonitorPasswordChanged));
+
+        private static void OnMonitorPasswordChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var passwordBox = (d as PasswordBox);
+
+            if (passwordBox == null)
+                return;
+
+            passwordBox.PasswordChanged += PasswordBox_PasswordChanged;
+
+            if ((bool)e.NewValue)
+            {
+                SetHasText(passwordBox);
+                passwordBox.PasswordChanged += PasswordBox_PasswordChanged;
+            }
+        }
+
+        private static void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            SetHasText((PasswordBox)sender);
+        }
+
+        public static void SetMonitorPassword(PasswordBox element, bool value)
+        {
+            element.SetValue(MonitorPasswordProperty, value);
+        }
+
+        public static bool GetMonitorPassword(PasswordBox element)
+        {
+            return (bool)element.GetValue(MonitorPasswordProperty);
+        }
+
+        public static readonly DependencyProperty HasTextProperty = 
+            DependencyProperty.RegisterAttached("HasText", typeof(bool), typeof(PasswordBoxPropertiesForTutorial), new PropertyMetadata(false));
+
+        private static void SetHasText(PasswordBox element)
+        {
+            element.SetValue(HasTextProperty, element.SecurePassword.Length > 0);
+        }
+
+        public static bool GetHasText (PasswordBox element)
+        {
+            return (bool)element.GetValue(HasTextProperty);
+        }
+
+        #endregion
+
+    }
+}
---
+++ Fasetto.Word/Fasetto.Word/ValueConverters/BooleanToVisibiltyConvertor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+
+namespace Fasetto.Word
+{
+    /// <summary>
+    /// A converter that takes in a boolean and returna a <see cref="Visibility"/>
+    /// </summary>
+    public class BooleanToVisibiltyConvertor : BaseValueConverter<BooleanToVisibiltyConvertor>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((bool)value) ? Visibility.Hidden : Visibility.Visible;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
