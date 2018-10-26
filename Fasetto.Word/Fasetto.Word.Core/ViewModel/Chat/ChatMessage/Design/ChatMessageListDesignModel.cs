39125dc6c5f5497b43e8abbab9a3788a1636849e
Prashant Baher <prashantbaher@gmail.com>
2018-10-26 18:29:30 +0530

    Adaptive Control Design with View Model Binding
    Completed till 27:00 min.
    Need to complete at home.

---
+++ Fasetto.Word/Fasetto.Word.Core/ViewModel/Chat/ChatMessage/Design/ChatMessageListDesignModel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fasetto.Word.Core
+{
+    /// <summary>
+    /// The design time data for a <see cref="ChatMessageListViewModel"/>
+    /// </summary>
+    public class ChatMessageListDesignModel : ChatMessageListViewModel
+    {
+        #region Singlton
+
+        /// <summary>
+        /// A single instance of the design model
+        /// </summary>
+        public static ChatMessageListDesignModel Instance => new ChatMessageListDesignModel();
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public ChatMessageListDesignModel()
+        {
+            Items = new List<ChatMessageListItemViewModel>
+            {
+                new ChatMessageListItemViewModel
+                {
+                    Initials = "PL",
+                    SenderName = "Parnell",
+                    Message = "I am about to wipe the old server. We need to update the old server to Windows 2016",
+                    ProfilePictureRGB = "3099c5",
+                    MessageSentTime = DateTimeOffset.UtcNow,
+                    SentByMe = false
+                },
+                new ChatMessageListItemViewModel
+                {
+                    Initials = "Prashant",
+                    SenderName = "PB",
+                    Message = "Let me know when you manage to spin the new 2016 server.",
+                    ProfilePictureRGB = "3099c5",
+                    MessageSentTime = DateTimeOffset.UtcNow,
+                    MessageReadTime = DateTimeOffset.UtcNow.Subtract(TimeSpan.FromDays(1.5)),
+                    SentByMe = false
+                },
+                new ChatMessageListItemViewModel
+                {
+                    Initials = "PL",
+                    SenderName = "Parnell",
+                    Message = "The new server is up. Go to 192.168.1.1 .\r\nUsername is Admin, password is P8ssw0rd!",
+                    ProfilePictureRGB = "3099c5",
+                    MessageSentTime = DateTimeOffset.UtcNow,
+                    SentByMe = false
+                },
+            };
+        }
+
+        #endregion
+    }
+}
---
+++ Fasetto.Word/Fasetto.Word.Core/ViewModel/Chat/ChatMessage/Design/ChatMessageListItemDesignModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Fasetto.Word.Core
+{
+    /// <summary>
+    /// The design-time data for a <see cref="ChatMessageListItemViewModel"/>
+    /// </summary>
+    public class ChatMessageListItemDesignModel : ChatMessageListItemViewModel
+    {
+        #region Singelton
+
+        // Below property is expand version of Instance property, which is the short hand version
+        // public static ChatListItemDesignModel Instance { get {return new ChatListItemDesignModel(); } }
+
+        /// <summary>
+        /// A single instance of the design model
+        /// </summary>
+        public static ChatMessageListItemDesignModel Instance => new ChatMessageListItemDesignModel();
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public ChatMessageListItemDesignModel()
+        {
+            Initials = "PB";
+            SenderName = "Prashant Baher";
+            Message = "Some design time visual text.";
+            ProfilePictureRGB = "3099c5";
+            SentByMe = true;
+            MessageSentTime = DateTime.UtcNow;
+            MessageReadTime = DateTimeOffset.UtcNow.Subtract(TimeSpan.FromDays(1.5));
+
+        }
+
+        #endregion
+    }
+}
